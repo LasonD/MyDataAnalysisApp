7d306a7f4205eff742f913c77134849b53e90869
Oleksandr Barylo <sbarylo20023@gmail.com>
2021-11-13 21:23:07 +0200

    Add more logging

--- ConsoleUI/TelegramTextProcessingServer.cs
+++ ConsoleUI/TelegramTextProcessingServer.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DataAnalysisLib.TextAnalysisVisualization;
 using DataAnalysisLib.TextAnalyzer;
+using log4net;
 using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
@@ -14,8 +16,11 @@ namespace ConsoleUI
 {
     public class TelegramTextProcessingServer : IUpdateHandler
     {
+        private const string ReportFileName = "Report.xlsx";
+
         private ExcelTextAnalysisVisualizer visualizer;
         private TextAnalyzer analyzer;
+        private readonly ILog log = LogManager.GetLogger(typeof(TelegramTextProcessingServer));
 
         public async Task HandleUpdate(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
@@ -26,30 +31,41 @@ namespace ConsoleUI
             catch (Exception e)
             {
                 await botClient.SendTextMessageAsync(update.Message.From.Id, e.StackTrace, cancellationToken: cancellationToken);
+
+                log.Error($"Exception while processing update from " +
+                          $"{update.Message.From.Username} {update.Message.From.Id}");
             }
         }
 
         private async Task HandleInternal(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
+            LogUpdate(update);
+
             if (update.Type is UpdateType.Message && update.Message.Type is MessageType.Document)
             {
-                var message = update.Message;
-
-                var sentFileStream = new MemoryStream();
-                var resultFileStream = new MemoryStream();
-
-                await botClient.GetInfoAndDownloadFileAsync(message.Document.FileId, sentFileStream, cancellationToken);
-
-                sentFileStream.Position = 0;
-
-                SetupAnalysisInfrastructure(sentFileStream, resultFileStream);
-
-                await visualizer.PlotAsync();
-
-                resultFileStream.Position = 0;
-
-                await botClient.SendDocumentAsync(message.From.Id, new InputOnlineFile(resultFileStream, "Report.xlsx"), cancellationToken: cancellationToken);
+                await HandleDocumentProcessingRequest(botClient, update, cancellationToken);
             }
+        }
+
+        private async Task HandleDocumentProcessingRequest(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+
+            var sentFileStream = new MemoryStream();
+            var resultFileStream = new MemoryStream();
+
+            await botClient.GetInfoAndDownloadFileAsync(message.Document.FileId, sentFileStream, cancellationToken);
+
+            sentFileStream.Position = 0;
+
+            SetupAnalysisInfrastructure(sentFileStream, resultFileStream);
+
+            await visualizer.PlotAsync();
+
+            resultFileStream.Position = 0;
+
+            await botClient.SendDocumentAsync(message.From.Id,
+                new InputOnlineFile(resultFileStream, ReportFileName), cancellationToken: cancellationToken);
         }
 
         public Task HandleError(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
@@ -65,6 +81,27 @@ namespace ConsoleUI
         {
             analyzer = new TextAnalyzer(source);
             visualizer = new ExcelTextAnalysisVisualizer(destination, analyzer);
+        }
+
+        private void LogUpdate(Update update)
+        {
+            var msg = update.Message;
+
+            var logBuilder = new StringBuilder($"Received update from user {msg.From.Username} {update.Message.From.Id}. ");
+
+            logBuilder.AppendLine($"Message type: {msg.Type}, ");
+
+            switch (update.Message.Type)
+            {
+                case MessageType.Text:
+                    logBuilder.AppendLine($"Message text: {msg.Text}, ");
+                    break;
+                case MessageType.Document:
+                    logBuilder.AppendLine($"Document: {msg.Document.FileName}, size {msg.Document.FileSize}");
+                    break;
+            }
+
+            log.Info(logBuilder.ToString());
         }
     }
 }
--- ConsoleUI/UsersNotifier.cs
+++ ConsoleUI/UsersNotifier.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using DAL.Data;
+using log4net;
 using Telegram.Bot;
 
 namespace ConsoleUI
@@ -9,6 +10,7 @@ namespace ConsoleUI
     {
         private readonly ITelegramBotClient botClient;
         private readonly TelegramDbContext context;
+        private readonly ILog log = LogManager.GetLogger(typeof(UsersNotifier));
 
         public UsersNotifier(ITelegramBotClient botClient, string connectionString)
         {
@@ -18,15 +20,20 @@ namespace ConsoleUI
 
         public async Task NotifyAllAsync(string notification)
         {
+            log.Debug($"Starting notification of users with message '{notification}'");
+
             foreach (var user in context.TelegramUsers)
             {
                 try
                 {
                     await botClient.SendTextMessageAsync(user.UserId, notification);
+
+                    log.Info($"Successfully notified user {user.Username} {user.UserId} with message '{notification}'");
                 }
                 catch (Exception e)
                 {
-                   
+                   log.Error($"Failed to send notification '{notification}' to user {user.Username} " +
+                             $"with id {user.UserId}, error message: {e.Message}");
                 }
             }
         }
